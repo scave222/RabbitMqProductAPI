2f30a169ba10e44c897d49d71dfc3c56b348faab
scave222 <okogbecaleb@gmail.com>
2024-11-28 08:06:53 +0100

    Add project files.

---
+++ RabbitMqProductAPI/Migrations/20241128063232_rabbpro.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RabbitMqProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class rabbpro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "RabbitMqProducts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RabbitMqProducts",
+                table: "RabbitMqProducts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RabbitMqProducts",
+                table: "RabbitMqProducts");
+
+            migrationBuilder.RenameTable(
+                name: "RabbitMqProducts",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "ProductId");
+        }
+    }
+}
